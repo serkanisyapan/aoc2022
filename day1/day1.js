600d51adf0393ecb359682c738122498d67f942a
serkanisyapan <isyapan15@itu.edu.tr>
2022-12-01 12:52:14 +0300

    day1 part2

--- day1/day1.js
+++ day1/day1.js
@@ -1,16 +1,15 @@
 import { data } from "./input.js";
 let calorieCount = 0;
 let maxCalorie = 0;
+let calorieArray = [];
 for (let calories of data) {
   if (calories === "") {
     calorieCount = 0;
   } else {
     let calorieNumber = parseInt(calories);
     calorieCount += calorieNumber;
-  }
-
-  if (calorieCount > maxCalorie) {
-    maxCalorie = calorieCount;
+    calorieArray.push(calorieCount);
   }
 }
-console.log(maxCalorie);
+let sortArray = calorieArray.sort((a, b) => b - a);
+console.log(sortArray[0] + sortArray[1] + sortArray[2]);
