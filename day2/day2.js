d801c3df6726d84d3b2367677203c18a5a524821
serkanisyapan <isyapan15@itu.edu.tr>
2022-12-02 13:53:40 +0300

    adds day2 part2 solution

--- day2/day2.js
+++ day2/day2.js
@@ -9,23 +9,44 @@ const scoreTable = {
   win: 6,
 };
 
+// for (let choices of data) {
+//   const [elf, user] = choices.split(" ");
+//   if (user === "X") userScore += scoreTable.rock;
+//   if (user === "Y") userScore += scoreTable.paper;
+//   if (user === "Z") userScore += scoreTable.scissors;
+//   if (
+//     (elf === "A" && user === "X") ||
+//     (elf === "B" && user === "Y") ||
+//     (elf === "C" && user === "Z")
+//   ) {
+//     userScore += scoreTable.draw;
+//   }
+//   if (
+//     (elf === "A" && user === "Y") ||
+//     (elf === "B" && user === "Z") ||
+//     (elf === "C" && user === "X")
+//   ) {
+//     userScore += scoreTable.win;
+//   }
+// }
+
 for (let choices of data) {
   const [elf, user] = choices.split(" ");
-  if (user === "X") userScore += scoreTable.rock;
-  if (user === "Y") userScore += scoreTable.paper;
-  if (user === "Z") userScore += scoreTable.scissors;
-  if (
-    (elf === "A" && user === "X") ||
-    (elf === "B" && user === "Y") ||
-    (elf === "C" && user === "Z")
-  ) {
+  if (user === "X") {
+    if (elf === "A") userScore += scoreTable.scissors;
+    if (elf === "B") userScore += scoreTable.rock;
+    if (elf === "C") userScore += scoreTable.paper;
+  }
+  if (user === "Y") {
+    if (elf === "A") userScore += scoreTable.rock;
+    if (elf === "B") userScore += scoreTable.paper;
+    if (elf === "C") userScore += scoreTable.scissors;
     userScore += scoreTable.draw;
   }
-  if (
-    (elf === "A" && user === "Y") ||
-    (elf === "B" && user === "Z") ||
-    (elf === "C" && user === "X")
-  ) {
+  if (user === "Z") {
+    if (elf === "A") userScore += scoreTable.paper;
+    if (elf === "B") userScore += scoreTable.scissors;
+    if (elf === "C") userScore += scoreTable.rock;
     userScore += scoreTable.win;
   }
 }
