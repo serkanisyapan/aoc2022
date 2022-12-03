9ced0d4ce43391bcebc58bb44df09bab3347a703
serkanisyapan <isyapan15@itu.edu.tr>
2022-12-03 13:53:09 +0300

    day3 part2 solution

--- day3/day3part2.js
+++ day3/day3part2.js
@@ -55,22 +55,33 @@ let scoreData = {
   Z: 52,
 };
 
-const findMatch = (stringOne, stringTwo) => {
+const findMatch = (stringOne, stringTwo, stringThree) => {
   let matchCharacter = "";
   for (let char in stringOne) {
-    if (stringTwo.includes(stringOne[char])) {
+    if (
+      stringTwo.includes(stringOne[char]) &&
+      stringThree.includes(stringOne[char])
+    ) {
       matchCharacter = `${stringOne[char]}`;
     }
   }
   return matchCharacter;
 };
 
-let totalRuckSack = 0;
-for (let rucksack of data) {
-  const [first, second] = [
-    rucksack.slice(0, rucksack.length / 2),
-    rucksack.slice(rucksack.length / 2),
-  ];
-  let character = findMatch(first, second);
-  totalRuckSack += scoreData[character];
+let arrayofThrees = [];
+let array = [];
+for (let i = 0; i < data.length; i += 3) {
+  array.push(data[i]);
+  array.push(data[i + 1]);
+  array.push(data[i + 2]);
+  arrayofThrees.push(array);
+  array = [];
 }
+
+let totalRuckSack = 0
+for (let threes of arrayofThrees) {
+  const [stringOne, stringTwo, stringThree] = threes;
+  let character = findMatch(stringOne, stringTwo, stringThree)
+  totalRuckSack += scoreData[character] 
+}
+console.log(totalRuckSack)
