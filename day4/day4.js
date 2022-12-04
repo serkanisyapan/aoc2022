2ac91caaba0bd949bf5bc69d722afe025a2e0484
serkanisyapan <isyapan15@itu.edu.tr>
2022-12-04 15:23:54 +0300

    some refactoring to solution 1

--- day4/day4.js
+++ day4/day4.js
@@ -3,18 +3,17 @@ import { data } from "./day4input.js";
 let totalContainer = 0;
 for (let pairs of data) {
   const [firstPair, secondPair] = pairs.split(",");
-  let firstPairSplit = firstPair.split("-");
-  let secondPairSplit = secondPair.split("-");
-  for (let i = 0; i < firstPairSplit.length; i += 2) {
-    let l1 = parseInt(firstPairSplit[i]);
-    let r1 = parseInt(firstPairSplit[i + 1]);
-    let l2 = parseInt(secondPairSplit[i]);
-    let r2 = parseInt(secondPairSplit[i + 1]);
-    if (l1 <= l2 && r2 <= r1) {
-      totalContainer += 1;
-    } else if (l2 <= l1 && r1 <= r2) {
-      totalContainer += 1;
-    }
+  let [l1, r1] = firstPair.split("-");
+  let [l2, r2] = secondPair.split("-");
+  l1 = parseInt(l1);
+  l2 = parseInt(l2);
+  r1 = parseInt(r1);
+  r2 = parseInt(r2);
+  if (l1 <= l2 && r2 <= r1) {
+    totalContainer += 1;
+  } else if (l2 <= l1 && r1 <= r2) {
+    totalContainer += 1;
   }
 }
+
 console.log(totalContainer);
--- day4/day4part2.js
+++ day4/day4part2.js
@@ -1,1 +1,20 @@
 import { data } from "./day4input.js";
+
+let totalOverlap = 0;
+for (let pairs of data) {
+  const [firstPair, secondPair] = pairs.split(",");
+  let firstPairSplit = firstPair.split("-");
+  let secondPairSplit = secondPair.split("-");
+  for (let i = 0; i < firstPairSplit.length; i += 2) {
+    let l1 = parseInt(firstPairSplit[i]);
+    let r1 = parseInt(firstPairSplit[i + 1]);
+    let l2 = parseInt(secondPairSplit[i]);
+    let r2 = parseInt(secondPairSplit[i + 1]);
+    if (l1 <= l2 && r2 <= r1) {
+      totalOverlap += 1;
+    } else if (l2 <= l1 && r1 <= r2) {
+      totalOverlap += 1;
+    }
+  }
+}
+console.log(totalOverlap);
