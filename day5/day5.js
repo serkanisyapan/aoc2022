b72541f689d62095146d2578d9c8b97b0b4aa82e
serkanisyapan <isyapan15@itu.edu.tr>
2022-12-05 13:41:42 +0300

    day5 part1 solutiom

--- day5/day5.js
+++ day5/day5.js
@@ -1,1 +1,27 @@
 import { data } from "./day5input.js";
+
+let startPosition = [
+  ["Q", "F", "M", "R", "L", "W", "C", "V"],
+  ["D", "Q", "L"],
+  ["P", "S", "R", "G", "W", "C", "N", "B"],
+  ["L", "C", "D", "H", "B", "Q", "G"],
+  ["V", "G", "L", "F", "Z", "S"],
+  ["D", "G", "N", "P"],
+  ["D", "Z", "P", "V", "F", "C", "W"],
+  ["C", "P", "D", "M", "S"],
+  ["Z", "N", "W", "T", "V", "M", "P", "C"],
+];
+
+for (let crates of data) {
+  const splitCrates = crates.split(" ");
+  const quantity = parseInt(splitCrates[1]);
+  const from = parseInt(splitCrates[3]) - 1;
+  const to = parseInt(splitCrates[5]) - 1;
+  for (let i = 0; i < quantity; i++) {
+    startPosition[to].push(startPosition[from].pop());
+  }
+}
+
+for (let lastIndex of startPosition) {
+  console.log(lastIndex[lastIndex.length - 1]);
+}
